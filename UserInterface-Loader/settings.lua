7fe968a546f83c0aba0694a9534ac9ac79a0a699
RedGamer12 <73015566+RedGamer12@users.noreply.github.com>
2023-06-17 18:35:14 +0700

    Update settings.lua

--- UserInterface-Loader/settings.lua
+++ UserInterface-Loader/settings.lua
@@ -3,35 +3,35 @@ local DataFunctions = {}
 local Http = game:GetService("HttpService")
 
 function Data.new(name, data)
-	if not isfolder(name) then
-		makefolder(name)
-	end
+    if not isfolder(name) then
+        makefolder(name)
+    end
 
-    local savedData = isfile(name.."/settings.json") and Http:JSONDecode(readfile(name.."/Settings.json"))
-    
+    local savedData = isfile(name .. "/settings.json") and Http:JSONDecode(readfile(name .. "/Settings.json"))
+
     if savedData then
-        for i,v in pairs(data) do
+        for i, v in pairs(data) do
             if not savedData[i] then
                 savedData[i] = v
             end
         end
     end
 
-	return setmetatable({
-		Data = savedData or data,
-		FolderName = name
-	}, {
-		__index = DataFunctions
-	})
+    return setmetatable({
+        Data = savedData or data,
+        FolderName = name
+    }, {
+        __index = DataFunctions
+    })
 end
 
 function DataFunctions:Set(name, value)
-	self.Data[name] = value
-	writefile(self.FolderName.."/Settings.json", Http:JSONEncode(self.Data))
+    self.Data[name] = value
+    writefile(self.FolderName .. "/Settings.json", Http:JSONEncode(self.Data))
 end
 
 function DataFunctions:Get(name)
-	return self.Data[name]
+    return self.Data[name]
 end
 
 return Data

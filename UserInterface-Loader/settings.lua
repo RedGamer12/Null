218c8880dece08d51810231748576f0607de6f9b
RedGamer12 <73015566+RedGamer12@users.noreply.github.com>
2023-06-18 10:07:31 +0700

    Update settings.lua

--- UserInterface-Loader/settings.lua
+++ UserInterface-Loader/settings.lua
@@ -1,45 +1,37 @@
-local function mergeTable(t1, t2) -- Hàm gộp 2 table
-    for k, v in pairs(t2) do
-        if type(v) == "table" and type(t1[k]) == "table" then
-            mergeTable(t1[k], v)
-        else
-            t1[k] = v
-        end
-    end
-end
-
 local Data = {}
 local DataFunctions = {}
 local Http = game:GetService("HttpService")
 
 function Data.new(name, data)
-    if not isfolder(name) then
-        makefolder(name)
+	if not isfolder(name) then
+		makefolder(name)
+	end
+
+    local savedData = isfile(name.."/settings.json") and Http:JSONDecode(readfile(name.."/Settings.json"))
+    
+    if savedData then
+        for i,v in pairs(data) do
+            if not savedData[i] then
+                savedData[i] = v
+            end
+        end
     end
 
-    local savedData = {}
-    if isfile(name .. "/settings.json") then
-        savedData = Http:JSONDecode(readfile(name .. "/Settings.json"))
-        mergeTable(savedData, data) -- Gộp dữ liệu lưu trữ và dữ liệu mặc định
-    else
-        savedData = data
-    end
-
-    return setmetatable({
-        Data = savedData,
-        FolderName = name
-    }, {
-        __index = DataFunctions
-    })
+	return setmetatable({
+		Data = savedData or data,
+		FolderName = name
+	}, {
+		__index = DataFunctions
+	})
 end
 
 function DataFunctions:Set(name, value)
-    self.Data[name] = value
-    writefile(self.FolderName .. "/Settings.json", Http:JSONEncode(self.Data))
+	self.Data[name] = value
+	writefile(self.FolderName.."/Settings.json", Http:JSONEncode(self.Data))
 end
 
 function DataFunctions:Get(name)
-    return self.Data[name]
+	return self.Data[name]
 end
 
 return Data

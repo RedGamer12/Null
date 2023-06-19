c4f582d0daafaafc71fc5b5c94a10559e766b8c2
RedGamer12 <73015566+RedGamer12@users.noreply.github.com>
2023-06-19 17:13:25 +0700

    Update settings.lua

--- UserInterface-Loader/settings.lua
+++ UserInterface-Loader/settings.lua
@@ -1,54 +1,37 @@
 local Data = {}
 local DataFunctions = {}
-local HttpService = game:GetService("HttpService")
+local Http = game:GetService("HttpService")
 
-function Data.new(name, data, fileName)
-    if not isfolder(name) then
-        makefolder(name)
-    end
+function Data.new(name, data)
+	if not isfolder(name) then
+		makefolder(name)
+	end
 
-    local filePath = name .. "/" .. (fileName or "settings.json")
-    local savedData = isfile(filePath) and HttpService:JSONDecode(readfile(filePath))
-
+    local savedData = isfile(name.."/settings.json") and Http:JSONDecode(readfile(name.."/Settings.json"))
+    
     if savedData then
-        for key, value in pairs(data) do
-            if not savedData[key] then
-                savedData[key] = value
+        for i,v in pairs(data) do
+            if not savedData[i] then
+                savedData[i] = v
             end
         end
     end
 
-    return setmetatable({
-        Data = savedData or data,
-        FileName = fileName or "settings.json",
-        FolderName = name,
-    }, {
-        __index = Data,
-    })
+	return setmetatable({
+		Data = savedData or data,
+		FolderName = name
+	}, {
+		__index = DataFunctions
+	})
 end
 
-function Data:Set(key, value)
-    self.Data[key] = value
-    writefile(self.FolderName .. "/" .. self.FileName, HttpService:JSONEncode(self.Data))
+function DataFunctions:Set(name, value)
+	self.Data[name] = value
+	writefile(self.FolderName.."/Settings.json", Http:JSONEncode(self.Data))
 end
 
-function Data:Get(key)
-    return self.Data[key]
+function DataFunctions:Get(name)
+	return self.Data[name]
 end
 
-function Data:Load()
-    local filePath = self.FolderName .. "/" .. self.FileName
-
-    if isfile(filePath) then
-        local success, result = pcall(function()
-            return HttpService:JSONDecode(readfile(filePath))
-        end)
-
-        if success then
-            self.Data = result or {}
-        else
-            warn("Error loading data: ", result)
-        end
-    end
-end
 return Data

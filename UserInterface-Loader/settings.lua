2e8b10ebcda784690e4d4edf29ed502855d2102c
RedGamer12 <73015566+RedGamer12@users.noreply.github.com>
2023-06-19 17:18:23 +0700

    Update settings.lua

--- UserInterface-Loader/settings.lua
+++ UserInterface-Loader/settings.lua
@@ -2,12 +2,13 @@ local Data = {}
 local DataFunctions = {}
 local Http = game:GetService("HttpService")
 
-function Data.new(name, data)
+function Data.new(name, data, fileName)
 	if not isfolder(name) then
 		makefolder(name)
 	end
 
-    local savedData = isfile(name.."/settings.json") and Http:JSONDecode(readfile(name.."/Settings.json"))
+	local filePath = name .. "/" .. (fileName or "settings.json")
+    local savedData = isfile(filePath) and Http:JSONDecode(readfile(filePath))
     
     if savedData then
         for i,v in pairs(data) do
@@ -19,7 +20,8 @@ function Data.new(name, data)
 
 	return setmetatable({
 		Data = savedData or data,
-		FolderName = name
+		FolderName = name,
+        FileName = fileName or "settings.json"
 	}, {
 		__index = DataFunctions
 	})
@@ -27,11 +29,27 @@ end
 
 function DataFunctions:Set(name, value)
 	self.Data[name] = value
-	writefile(self.FolderName.."/Settings.json", Http:JSONEncode(self.Data))
+	writefile(self.FolderName.."/"..self.FileName, Http:JSONEncode(self.Data))
 end
 
 function DataFunctions:Get(name)
 	return self.Data[name]
+end
+
+function DataFunctions:Load()
+    local filePath = self.FolderName .. "/" .. self.FileName
+
+    if isfile(filePath) then
+        local success, result = pcall(function()
+            return Http:JSONDecode(readfile(filePath))
+        end)
+
+        if success then
+            self.Data = result or {}
+        else
+            warn("Error loading data: ", result)
+        end
+    end
 end
 
 return Data
